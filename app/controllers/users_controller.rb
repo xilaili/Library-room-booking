4c3b1b473fccabdb30b869ae3c6beb7cbf5a0a9e
Xilai <xli47@ncsu.edu>
2016-09-22 16:38:16 +0000

    users first draft

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,74 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy, :show]
+  before_action :correct_user,   only: [:edit, :update, :show]
+  before_action :admin_user,     only: :destroy
+  def new
+    @user = User.new
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      log_in @user
+      redirect_to @user
+      flash[:success] = "Welcome to the Sample App!"
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        redirect_to(users_url) 
+        flash[:danger] = "Permission denied."
+      end
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+end
